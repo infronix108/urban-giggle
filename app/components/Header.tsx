d4cafd6425cb84fd83964847dc8fdd15ba227496
infronix108 <infronix108@gmail.com>
2025-03-01 08:37:54 +0530

    Update Header.tsx
    enhancing logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
-import Image from "next/image";
+import Image from "next/image"; 
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);

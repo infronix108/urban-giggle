64a69894af2a957c1949faabb8fdb261b2497be0
infronix108 <infronix108@gmail.com>
2025-03-05 09:52:30 +0530

    dependency

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false, // Disable SWC minification
+  images: {
+    domains: ['images.unsplash.com'], // Allow images from Unsplash
+  },
   experimental: {
     // Disable experimental features that might cause issues
     webpackBuildWorker: false,
     parallelServerCompiles: false,
-    parallelServerBuildTraces: false
-  }
-}
+    parallelServerBuildTraces: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
